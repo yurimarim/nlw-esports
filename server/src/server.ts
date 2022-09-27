b2120887ed1393b60223379348c882449c150001
yurimarim <yurimarim@hotmail.com>
2022-09-27 20:09:44 -0300

    server

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,10 @@ app.get('/ads', (request, response) => {
   return response.json([
     { id: 1, name: 'Anúncio 1' },
     { id: 2, name: 'Anúncio 2' },
-    { id: 3, name: 'Anúncio 3' }
+    { id: 3, name: 'Anúncio 3' },
+    { id: 4, name: 'Anúncio 4' },
+    { id: 5, name: 'Anúncio 5' },
+    { id: 6, name: 'Anúncio 6' },
   ])
 })
 
